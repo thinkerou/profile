78348f18f1d260508b4956845de795e1c9568225
thinkerou <thinkerou@gmail.com>
2018-05-29 22:44:34 +0800

    add api

--- router/router.go
+++ router/router.go
@@ -1,2 +1,18 @@
 package router
 
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/thinkerou/your-profile-about-github/server"
+)
+
+func Load() http.Handler {
+	r := gin.Default()
+
+	r.GET("/api/user/:user", server.GetUserProfile)
+
+	return r
+}
+
--- server/user.go
+++ server/user.go
@@ -1,1 +1,12 @@
 package server
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"github.com/thinkerou/your-profile-about-github/model"
+)
+
+func GetUserProfile(c *gin.Context) {
+}
