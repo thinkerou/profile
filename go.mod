d4c732a27f3d8a5df0bdb7c38247f5c61c6030ff
thinkerou <thinkerou@gmail.com>
2019-05-26 14:09:18 +0800

    init and test github api

--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module github.com/thinkerou/profile
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/gin-gonic/gin v1.4.0
+	github.com/google/go-github v17.0.0+incompatible
+	github.com/google/go-querystring v1.0.0 // indirect
+	golang.org/x/oauth2 v0.0.0-00010101000000-000000000000
+)
+
+replace golang.org/x/oauth2 => github.com/golang/oauth2 v0.0.0-20190523182746-aaccbc9213b0
--- main.go
+++ main.go
@@ -1,15 +1,43 @@
 package main
 
 import (
-	"net/http"
+	"context"
+	"fmt"
+	// "net/http"
+	"os"
 
-	"github.com/gin-gonic/gin"
+	// "github.com/gin-gonic/gin"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func main() {
-	r := gin.Default()
+	/*r := gin.Default()
 	r.GET("/welcome", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"msg": "Hello world!"})
 	})
-	r.Run()
+	r.Run()*/
+	token := ""
+	token = os.Getenv("GITHUB_TOKEN")
+	println(token)
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	orgs, _, err := client.Organizations.List(context.Background(), "thinkerou", nil)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return
+	}
+
+	for i, org := range orgs {
+		fmt.Printf("%v. %v\n", i+1, org.GetLogin())
+	}
+	/*repos, _, err := client.Repositories.List(ctx, "", nil)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+	}
+	for i, repo := range repos {
+		fmt.Printf("%v. %v\n", i+1, repo)
+	}*/
 }
