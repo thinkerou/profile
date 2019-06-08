31e3c2229e6ea217df13e31fb2511939b40bbd9a
thinkerou <thinkerou@gmail.com>
2019-06-08 22:20:15 +0800

    add user model

--- model/user.go
+++ model/user.go
@@ -1,1 +1,16 @@
 package model
+
+import "github.com/google/go-github/github"
+
+type UserProfile struct {
+	user                github.User
+	quarterCommitCount  map[string]uint
+	langRepoCount       map[string]uint
+	langStarCount       map[string]uint
+	langCommitCount     map[string]uint
+	repoCommitCount     map[string]uint
+	repoStarCount       map[string]uint
+	repoCommitCountDesc map[string]string
+	repoStarCountDesc   map[string]string
+	timeStamp           int64
+}
