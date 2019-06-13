69a4be3158badddccf20e3a815a9faf3886b272d
thinkerou <thinkerou@gmail.com>
2019-06-13 23:18:36 +0800

    add json for user model

--- model/user.go
+++ model/user.go
@@ -3,14 +3,14 @@ package model
 import "github.com/google/go-github/github"
 
 type UserProfile struct {
-	user                github.User
-	quarterCommitCount  map[string]uint
-	langRepoCount       map[string]uint
-	langStarCount       map[string]uint
-	langCommitCount     map[string]uint
-	repoCommitCount     map[string]uint
-	repoStarCount       map[string]uint
-	repoCommitCountDesc map[string]string
-	repoStarCountDesc   map[string]string
-	timeStamp           int64
+	user                github.User       `json:"user"`
+	quarterCommitCount  map[string]uint   `json:"quarterCommitCount"`
+	langRepoCount       map[string]uint   `json:"langRepoCount"`
+	langStarCount       map[string]uint   `json:"langStarCount"`
+	langCommitCount     map[string]uint   `json:"langCommitCount"`
+	repoCommitCount     map[string]uint   `json:"repoCommitCount"`
+	repoStarCount       map[string]uint   `json:"repoStarCount"`
+	repoCommitCountDesc map[string]string `json:"repoCommitCountDesc"`
+	repoStarCountDesc   map[string]string `json:"repoStarCountDesc"`
+	timeStamp           int64             `json:"timeStamp"`
 }
