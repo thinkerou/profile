b62b5afda4d2ad29a4b3499b8d872f5e4d1f3ac8
thinkerou <thinkerou@gmail.com>
2019-06-16 22:15:06 +0800

    add some test code

--- model/user.go
+++ model/user.go
@@ -3,14 +3,14 @@ package model
 import "github.com/google/go-github/github"
 
 type UserProfile struct {
-	user                github.User       `json:"user"`
-	quarterCommitCount  map[string]uint   `json:"quarterCommitCount"`
-	langRepoCount       map[string]uint   `json:"langRepoCount"`
-	langStarCount       map[string]uint   `json:"langStarCount"`
-	langCommitCount     map[string]uint   `json:"langCommitCount"`
-	repoCommitCount     map[string]uint   `json:"repoCommitCount"`
-	repoStarCount       map[string]uint   `json:"repoStarCount"`
-	repoCommitCountDesc map[string]string `json:"repoCommitCountDesc"`
-	repoStarCountDesc   map[string]string `json:"repoStarCountDesc"`
-	timeStamp           int64             `json:"timeStamp"`
+	User                github.User       `json:"user"`
+	QuarterCommitCount  map[string]uint   `json:"quarterCommitCount"`
+	LangRepoCount       map[string]uint   `json:"langRepoCount"`
+	LangStarCount       map[string]uint   `json:"langStarCount"`
+	LangCommitCount     map[string]uint   `json:"langCommitCount"`
+	RepoCommitCount     map[string]uint   `json:"repoCommitCount"`
+	RepoStarCount       map[string]uint   `json:"repoStarCount"`
+	RepoCommitCountDesc map[string]string `json:"repoCommitCountDesc"`
+	RepoStarCountDesc   map[string]string `json:"repoStarCountDesc"`
+	TimeStamp           int64             `json:"timeStamp"`
 }
--- server/user.go
+++ server/user.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/go-github/github"
+	"github.com/thinkerou/profile/model"
 	"golang.org/x/oauth2"
 )
 
@@ -19,12 +21,12 @@ func GetUserProfile(c *gin.Context) {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	users, _, err := client.Users.Get(ctx, username)
+	user, _, err := client.Users.Get(ctx, username)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
 		return
 	}
-	println(users.String())
+	println(user.String())
 	// c.JSON(http.StatusOK, gin.H{"email": *user.Email})
 
 	var repos []*github.Repository
@@ -47,5 +49,64 @@ func GetUserProfile(c *gin.Context) {
 		}
 	}
 	println(len(repos))
-}
 
+	var repoCommits = make(map[*github.Repository][]*github.RepositoryCommit)
+	var langRepoGrouping = make(map[string][]*github.Repository)
+	for _, repo := range repos {
+		if repo.Language == nil {
+			r, ok := langRepoGrouping["Unknown"]
+			if !ok {
+				langRepoGrouping["Unknown"] = []*github.Repository{repo}
+			} else {
+				langRepoGrouping["Unknown"] = append(r, repo)
+			}
+		} else {
+			r, ok := langRepoGrouping[*repo.Language]
+			if !ok {
+				langRepoGrouping[*repo.Language] = []*github.Repository{repo}
+			} else {
+				langRepoGrouping[*repo.Language] = append(r, repo)
+			}
+		}
+
+		var cs []*github.RepositoryCommit
+		i := 0
+		for {
+			i += 1
+			opt := &github.CommitsListOptions{Author: username, ListOptions: github.ListOptions{Page: i}}
+			commits, _, _ := client.Repositories.ListCommits(ctx, *repo.Owner.Login, *repo.Name, opt)
+			if len(commits) == 0 {
+				repoCommits[repo] = cs
+				break
+			}
+			cs = append(cs, commits...)
+		}
+	}
+	println(len(repoCommits))
+	println(len(langRepoGrouping))
+
+	var langRepoCount = make(map[string]uint)
+	// var langStarCount = make(map[string]uint)
+	// var langCommitCount = make(map[string]uint)
+	for k, v := range langRepoGrouping {
+		langRepoCount[k] = uint(len(v))
+		//
+	}
+
+	// var repoCommitCount = make(map[string]uint)
+	// var repoStarCount = make(map[string]uint)
+
+	uf := model.UserProfile{
+		User:                *user,
+		QuarterCommitCount:  nil,
+		LangRepoCount:       langRepoCount,
+		LangStarCount:       nil,
+		LangCommitCount:     nil,
+		RepoCommitCount:     nil,
+		RepoCommitCountDesc: nil,
+		RepoStarCountDesc:   nil,
+		TimeStamp:           time.Now().Unix(),
+	}
+
+	c.JSON(http.StatusOK, gin.H{"msg": uf})
+}
