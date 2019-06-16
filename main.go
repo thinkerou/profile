0f735bdb8406b862ad257234796d67c7f285ff23
thinkerou <thinkerou@gmail.com>
2019-06-16 19:33:55 +0800

    add some test code

--- main.go
+++ main.go
@@ -1,43 +1,24 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	// "net/http"
-	"os"
+	"net/http"
 
-	// "github.com/gin-gonic/gin"
-	"github.com/google/go-github/github"
-	"golang.org/x/oauth2"
+	"github.com/gin-gonic/gin"
+	"github.com/thinkerou/profile/server"
 )
 
 func main() {
-	/*r := gin.Default()
-	r.GET("/welcome", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"msg": "Hello world!"})
-	})
-	r.Run()*/
-	token := ""
-	token = os.Getenv("GITHUB_TOKEN")
-	println(token)
-	ctx := context.Background()
-	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
-	tc := oauth2.NewClient(ctx, ts)
-	client := github.NewClient(tc)
-	orgs, _, err := client.Organizations.List(context.Background(), "thinkerou", nil)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-		return
-	}
+	gin.SetMode(gin.DebugMode)
 
-	for i, org := range orgs {
-		fmt.Printf("%v. %v\n", i+1, org.GetLogin())
-	}
-	/*repos, _, err := client.Repositories.List(ctx, "", nil)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
-	}
-	for i, repo := range repos {
-		fmt.Printf("%v. %v\n", i+1, repo)
-	}*/
+	r := gin.Default()
+
+	// r.LoadHTMLGlob("resources/template/*")
+
+	r.GET("/api/user/:user", server.GetUserProfile)
+
+	r.Run()
+}
+
+func render(c *gin.Context, data gin.H, templateName string) {
+	c.JSON(http.StatusOK, data["payload"])
 }
--- server/user.go
+++ server/user.go
@@ -1,17 +1,51 @@
 package server
 
 import (
+	"context"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
-
-	"github.com/thinkerou/profile/model"
+	"github.com/google/go-github/github"
+	"golang.org/x/oauth2"
 )
 
 func GetUserProfile(c *gin.Context) {
-	user := c.Param("user")
+	username := c.Param("user")
 
+	token := os.Getenv("GITHUB_TOKEN")
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	users, _, err := client.Users.Get(ctx, username)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+		return
+	}
+	println(users.String())
+	// c.JSON(http.StatusOK, gin.H{"email": *user.Email})
+
+	var repos []*github.Repository
+	i := 0
+	for {
+		i += 1
+		opt := &github.RepositoryListOptions{ListOptions: github.ListOptions{Page: i}}
+		repo, _, err := client.Repositories.List(ctx, username, opt)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"msg": err.Error()})
+			return
+		}
+		if len(repo) == 0 {
+			break
+		}
+		for _, e := range repo {
+			if !*e.Fork && *e.Size != 0 {
+				repos = append(repos, e)
+			}
+		}
+	}
+	println(len(repos))
 }
 
-func RenderUserProfile(c *gin.Context) {
-}
